f6584b1571ea1a5f25c01723b0d0e78aba84b031
sanket <sanketsabale9767@gmail.com>
2023-02-15 02:52:41 +0530

    Working done on basic files like fonts and colors

---
+++ src/theme/fonts.ts
@@ -0,0 +1,20 @@
+/* eslint-disable prettier/prettier */
+const size = {
+  xs: 10,
+  sm: 12,
+  default: 14,
+  md: 17,
+  lg: 20,
+  xlg: 24,
+  xxlg: 30,
+};
+
+const weight = {
+  full: '900',
+  semi: '600',
+  bold: 'bold',
+  normal: 'normal',
+  thin: '400',
+};
+
+export default {size, weight};
